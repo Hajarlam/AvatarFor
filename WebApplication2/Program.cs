4329c86ee3510d212639101ebc25e4dd2b6dd2de
Hajar Lamharzi <Hajarlamharzi@gmail.com>
2026-07-17 01:31:13 +0100

    refactor: migrate database provider from SQL Server to SQLite for cross-platform Docker support

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddSession(options =>
 
 // Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
